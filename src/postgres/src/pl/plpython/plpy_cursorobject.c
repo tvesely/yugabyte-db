hackorum-dev/postgres

Consistently catch errors from Python _New() functions
Python Py*_New() functions can fail and return NULL in out-of-memory
conditions.  The previous code handled that inconsistently or not at
all.  This change organizes that better.  If we are in a function that
is called from Python, we just check for failure and return NULL
ourselves, which will cause any exception information to be passed up.
If we are called from PostgreSQL, we consistently create an "out of
memory" error.

src/pl/plpython/plpy_cursorobject.c
@@ -464,15 +464,20 @@ PLy_cursor_fetch(PyObject *self, PyObject *args)
 
 			Py_DECREF(ret->rows);
 			ret->rows = PyList_New(SPI_processed);
-
-			for (i = 0; i < SPI_processed; i++)
+			if (!ret->rows)
 			{
-				PyObject   *row = PLyDict_FromTuple(&cursor->result,
-													SPI_tuptable->vals[i],
-													SPI_tuptable->tupdesc);
-
-				PyList_SetItem(ret->rows, i, row);
+				Py_DECREF(ret);
+				ret = NULL;
 			}
+			else
+				for (i = 0; i < SPI_processed; i++)
+				{
+					PyObject   *row = PLyDict_FromTuple(&cursor->result,
+														SPI_tuptable->vals[i],
+														SPI_tuptable->tupdesc);
+
+					PyList_SetItem(ret->rows, i, row);
+				}
 		}
 
 		SPI_freetuptable(SPI_tuptable);

src/pl/plpython/plpy_spi.c
@@ -389,6 +389,8 @@ PLy_spi_execute_fetch_result(SPITupleTable *tuptable, uint64 rows, int status)
 	volatile MemoryContext oldcontext;
 
 	result = (PLyResultObject *) PLy_result_new();
+	if (!result)
+		return NULL;
 	Py_DECREF(result->status);
 	result->status = PyInt_FromLong(status);
 
@@ -435,15 +437,22 @@ PLy_spi_execute_fetch_result(SPITupleTable *tuptable, uint64 rows, int status)
 
 				Py_DECREF(result->rows);
 				result->rows = PyList_New(rows);
-
-				PLy_input_tuple_funcs(&args, tuptable->tupdesc);
-				for (i = 0; i < rows; i++)
+				if (!result->rows)
 				{
-					PyObject   *row = PLyDict_FromTuple(&args,
-														tuptable->vals[i],
-														tuptable->tupdesc);
+					Py_DECREF(result);
+					result = NULL;
+				}
+				else
+				{
+					PLy_input_tuple_funcs(&args, tuptable->tupdesc);
+					for (i = 0; i < rows; i++)
+					{
+						PyObject   *row = PLyDict_FromTuple(&args,
+															tuptable->vals[i],
+															tuptable->tupdesc);
 
-					PyList_SetItem(result->rows, i, row);
+						PyList_SetItem(result->rows, i, row);
+					}
 				}
 			}
 

tvesely/yugabyte-db

Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>

src/postgres/src/pl/plpython/plpy_cursorobject.c
@@ -457,17 +457,24 @@ PLy_cursor_fetch(PyObject *self, PyObject *args)
 
 			Py_DECREF(ret->rows);
 			ret->rows = PyList_New(SPI_processed);
-
-			PLy_input_setup_tuple(&cursor->result, SPI_tuptable->tupdesc,
-								  exec_ctx->curr_proc);
-
-			for (i = 0; i < SPI_processed; i++)
+			if (!ret->rows)
 			{
-				PyObject   *row = PLy_input_from_tuple(&cursor->result,
-													   SPI_tuptable->vals[i],
-													   SPI_tuptable->tupdesc);
+				Py_DECREF(ret);
+				ret = NULL;
+			}
+			else
+			{
+				PLy_input_setup_tuple(&cursor->result, SPI_tuptable->tupdesc,
+									  exec_ctx->curr_proc);
 
-				PyList_SetItem(ret->rows, i, row);
+				for (i = 0; i < SPI_processed; i++)
+				{
+					PyObject   *row = PLy_input_from_tuple(&cursor->result,
+														   SPI_tuptable->vals[i],
+														   SPI_tuptable->tupdesc);
+
+					PyList_SetItem(ret->rows, i, row);
+				}
 			}
 		}
 

src/postgres/src/pl/plpython/plpy_spi.c
@@ -360,6 +360,8 @@ PLy_spi_execute_fetch_result(SPITupleTable *tuptable, uint64 rows, int status)
 	volatile MemoryContext oldcontext;
 
 	result = (PLyResultObject *) PLy_result_new();
+	if (!result)
+		return NULL;
 	Py_DECREF(result->status);
 	result->status = PyInt_FromLong(status);
 
@@ -409,17 +411,24 @@ PLy_spi_execute_fetch_result(SPITupleTable *tuptable, uint64 rows, int status)
 
 				Py_DECREF(result->rows);
 				result->rows = PyList_New(rows);
-
-				PLy_input_setup_tuple(&ininfo, tuptable->tupdesc,
-									  exec_ctx->curr_proc);
-
-				for (i = 0; i < rows; i++)
+				if (!result->rows)
 				{
-					PyObject   *row = PLy_input_from_tuple(&ininfo,
-														   tuptable->vals[i],
-														   tuptable->tupdesc);
+					Py_DECREF(result);
+					result = NULL;
+				}
+				else
+				{
+					PLy_input_setup_tuple(&ininfo, tuptable->tupdesc,
+										  exec_ctx->curr_proc);
 
-					PyList_SetItem(result->rows, i, row);
+					for (i = 0; i < rows; i++)
+					{
+						PyObject   *row = PLy_input_from_tuple(&ininfo,
+															   tuptable->vals[i],
+															   tuptable->tupdesc);
+
+						PyList_SetItem(result->rows, i, row);
+					}
 				}
 			}
 