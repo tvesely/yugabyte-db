bd9230ac810c67ce34e2a6d3c5f63ef4832d5b3b
Taylor Vesely <tvesely@yugabyte.com>
2023-03-01 11:27:36 -0800

    Update the migrations to the latest version of the yb_lock_status function

--- src/yb/yql/pgwrapper/ysql_migrations/V36__15736__yb_lock_status_function.sql
+++ src/yb/yql/pgwrapper/ysql_migrations/V36__15736__yb_lock_status_function.sql
@@ -1,16 +1,16 @@
 SET LOCAL yb_non_ddl_txn_for_sys_tables_allowed TO true;
 
-INSERT INTO pg_catalog.pg_proc (
-  oid, proname, pronamespace, proowner, prolang, procost, prorows, provariadic, protransform,
-  prokind, prosecdef, proleakproof, proisstrict, proretset, provolatile, proparallel, pronargs,
-  pronargdefaults, prorettype, proargtypes, proallargtypes, proargmodes, proargnames,
-  proargdefaults, protrftypes, prosrc, probin, proconfig, proacl
-) VALUES
-  (8058, 'yb_lock_status', 11, 10, 12, 1, 1000, 0, '-', 'f', false, false, true, true,
-   'v', 's', 2, 0, 2249, '25 25', '{25,25,25,26,26,23,25,16,16,1114,1114,3802}', '{i,i,o,o,o,o,o,o,o,o,o,o}',
-   '{tablet_id,transaction_id,locktype,database,relation,pid,mode,granted,fastpath,waitstart,waitend,ybdetails}',
-   NULL, NULL, 'yb_lock_status', NULL, NULL, NULL)
-ON CONFLICT DO NOTHING;
+INSERT INTO pg_catalog.pg_proc (oid, proname, pronamespace, proowner, prolang, procost, prorows, provariadic,
+                                protransform,
+                                prokind, prosecdef, proleakproof, proisstrict, proretset, provolatile, proparallel,
+                                pronargs,
+                                pronargdefaults, prorettype, proargtypes, proallargtypes, proargmodes, proargnames,
+                                proargdefaults, protrftypes, prosrc, probin, proconfig, proacl)
+VALUES (8058, 'yb_lock_status', 11, 10, 12, 1, 1000, 0, '-', 'f', false, false, false, true,
+        'v', 's', 2, 0, 2249, '26 2950', '{26,2950,25,26,26,23,1009,16,16,1184,1184,25,25,2950,23,16,1009,1009,23,16}',
+        '{i,i,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}',
+        '{relation,transaction_id,locktype,database,relation,pid,mode,granted,single_shard,waitstart,waitend,node,tablet_id,transaction_id,subtransaction_id,is_explicit,hash_cols,range_cols,column_id,is_full_pk}',
+        NULL, NULL, 'yb_lock_status', NULL, NULL, NULL) ON CONFLICT DO NOTHING;
 
 -- Create dependency records for everything we (possibly) created.
 -- Since pg_depend has no OID or unique constraint, using PL/pgSQL instead.
